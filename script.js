e672111c18a4bbddd284e05746b0312002c8d8c3
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-09-04 13:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let rotationAngle = 0;
+
+function rotateLine() {
+    rotationAngle += 1;
+    line.style.transform = `rotate(${rotationAngle}deg)`;
+}
+
+// Set an interval to continuously rotate the line
+setInterval(rotateLine, 20);
